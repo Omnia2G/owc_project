e658a6256edef2b4ffccfd500b87597a8cdcdaa9
Omnia2G <zsombororosz@gmail.com>
2022-05-18 01:28:16 +0200

    db changed

--- src/api/Config.php
+++ src/api/Config.php
@@ -1,5 +1,10 @@
 <?php
-const DB_HOST = 'localhost';
-const DB_USER = 'root';
-const DB_PASS = '';
-const DB_NAME = 'owc';
+// const DB_HOST = 'localhost';
+// const DB_USER = 'root';
+// const DB_PASS = '';
+// const DB_NAME = 'owc';
+
+const DB_HOST = 'dbadmin.r2.websupport.sk';
+const DB_USER = 'orosz';
+const DB_PASS = 'Tv9Mw}r-C7';
+const DB_NAME = 'orosz';
--- src/api/try.php
+++ src/api/try.php
@@ -6,21 +6,9 @@ require_once "TestController.php";
 $userController = new UserController();
 $testController = new TestController();
 
-
-    $user = new User();
-        $user->setId(29);
-        $user->setFirstname('greg');
-        $user->setLastname('greg');
-        $user->setUsername('greg3');
-        $user->setEmail('greg@vfbfgb.vo');
-        $user->setPassword(password_hash("greggreg", PASSWORD_DEFAULT));
-        $user->setRole('teacher');
-        try{
-            $userController->editUser($user);
-            echo json_encode('Success');
-        }catch(PDOException $exception){
-            echo json_encode($exception->getMessage());
-        }
+echo "TRY ";
+$person = $userController->login('admin1@admin.com','','');
+var_dump($person);
 
 
 
