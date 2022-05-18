1a25afc7879a3376ac0e627eb5fad556f49038c4
Omnia2G <zsombororosz@gmail.com>
2022-05-18 03:01:53 +0200

    relative path

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -4,8 +4,8 @@ let timer;
 
 export default {
   async login(context, payload) {
-    let url = "http://localhost/owc_project/src/api/Actions.php";
-    //let url = "owc_project/src/api/Actions.php";
+    // let url = "http://localhost/owc_project/src/api/Actions.php";
+    let url = "src/api/Actions.php";
     const res = await fetch(url, {
       method: "POST",
       body: payload,
